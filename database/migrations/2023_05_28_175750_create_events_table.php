c94bdea3ba9a5173b02c31362c47ae26ec1d6c47
Ivancho <icastro@megalabs.com.co>
2023-05-28 13:04:02 -0500

    columns added to the migration

--- database/migrations/2023_05_28_175750_create_events_table.php
+++ database/migrations/2023_05_28_175750_create_events_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->date('start_date');
+            $table->string('end_date');
             $table->timestamps();
         });
     }
